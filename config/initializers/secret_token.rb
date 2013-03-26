d12b71156b48245b7d595b8a99188a710fc4c7bd
Angelie Angel <angelie_angel@informatics.edu.ph>
2013-03-26 15:14:35 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ndfy::Application.config.secret_token = 'd06fa9abcfa2055a0eed939cd75cca0cf08daed45b9aa57bf6e8b27557d710a194a3ba11dd90ae8aec14fb8273c752b913730170b6b315183573f99c401bf860'
